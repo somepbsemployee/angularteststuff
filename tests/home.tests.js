465f87360e5d7f095049a506a768be126d6c0275
Shaun Brown <Shaun.brown@peninsula-uk.com>
2015-01-20 12:59:33 +0000

    Added epicz00r stuff.

---
+++ tests/home.tests.js
@@ -0,0 +1,49 @@
+describe("Testing javascript tests", function() {
+  it("dsg fds gfdsdfg", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+
+  it("d  ffddf fdgdfg", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+
+  it(" df gdf fd df", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+
+  it("SIMON FUCKED UP AGAIN", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+
+  it("hhhhh", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+
+  it("tttt", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+
+  it("ttttttttt", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+
+  it("uuuuuu", function(done) {
+    expect(2).toBeGreaterThan(1);
+    expect('simon').toEqual('simon');
+    done();
+  });
+});
